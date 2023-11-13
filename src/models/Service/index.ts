70f63e61ba807098bcc872df3497843116ce2834
asvarga <asvarga@gmail.com>
2023-11-13 15:55:11 -0300

    20231113 AV: Service Model

---
+++ src/models/Service/index.ts
@@ -0,0 +1,24 @@
+
+import mongoose from "mongoose";
+const Schema = new mongoose.Schema( {
+    name: {
+        type: String
+        , required: true
+    }
+    , description: {
+        type: String
+        , required: true
+    }
+    , price: {
+        type: Number
+        , required: true
+    }
+    , image: {
+        type: String
+        , required: true
+    }
+}
+, { timestamps : true }
+);
+const ServiceModel = mongoose.model( "Service", Schema );
+export default ServiceModel;
