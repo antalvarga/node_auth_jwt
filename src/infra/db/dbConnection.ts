eed7c150bf3234f00017dbc5f4e1b3b9213b51a8
asvarga <asvarga@gmail.com>
2023-11-14 15:18:22 -0300

    20231114 AV: Correção no dbConnection

--- src/infra/db/dbConnection.ts
+++ src/infra/db/dbConnection.ts
@@ -2,16 +2,20 @@ import mongoose, {ConnectOptions} from "mongoose";
 
 const dbConnection = async() => {
 
-  const dbUri = process.env.DB_URI ? process.env.DB_URI : '';
+  // const dbUri = process.env.DB_URI ? process.env.DB_URI : '';
+  const dbUri = process.env.DB_URI2 ? process.env.DB_URI2 : '';
 
   try {
     
+    /*
     await mongoose.connect( dbUri, {
 
       useNewUrlParser: true
       , useUnifiedTopology: true
 
     } as ConnectOptions );
+    */
+    await mongoose.connect( dbUri )
     
     console.log( `{ message: Mongoose connected :: }` );
 
