754d90fd703f0ef0463bc31354dccce0642effa8
asvarga <asvarga@gmail.com>
2023-11-14 15:26:01 -0300

    20231114 AV: Transf da rota home do server para routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,10 +4,15 @@ import { ServiceControllerNew } from '../controllersNew';
 
 const Routes = Router();
 
+
+Routes.get( '/', (req, res) => { res.send( 'Obrigado Deus! Hello world funcionou...') });
+
 Routes.post( '/service'    , ServiceControllers.create );
 Routes.get ( '/service'    , ServiceControllers.index  );
 
 Routes.get ( '/serv'       , ServiceControllerNew.GetAll);
 Routes.post( '/serv'       , ServiceControllerNew.Create);
+
+
 
 export default Routes;
--- src/server.ts
+++ src/server.ts
@@ -18,11 +18,6 @@ app.use( cors() );
 app.use( express.json() );
 app.use( Routes );
 
-app.get( '/', (req, res) => {
-  console.log( 'Obrigado Deus! Hello world funcionou...');
-  res.send( 'Obrigado Deus! Hello world funcionou...');
-});
-
 app.listen( 3333, () => {
   console.log( '🚀 Server is running...' );
 })
